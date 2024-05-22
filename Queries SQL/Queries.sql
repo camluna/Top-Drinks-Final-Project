0076cc6c9611795c03b79930d61f517a7377df38
Camila Luna Bejarano <camila.lunabejarano@gmail.com>
2024-05-22 15:35:09 -0300

    Update Queries.sql

--- Queries SQL/Queries.sql
+++ Queries SQL/Queries.sql
@@ -2,16 +2,16 @@ USE TopDrinksDB
 
 -----------------------------------REVISIÓN DE LA TABLA PURCHASE PRICES-------------------------------------------------
 SELECT*
-FROM [2017PurchasePricesDec_Clean]
-ORDER BY VendorNumber
+FROM SalesFINAL12312016_Clean
+--ORDER BY VendorNumber
 
 -- Contar la cantidad de registros en la tabla PurchasePrices
 SELECT COUNT(*) AS TotalRegistros
-FROM [2017PurchasePricesDec_Clean];
+FROM InvoicePurchases12312016_Clean;
 
 -- Contar la cantidad de valores únicos en la columna PONumber PurchasePrices
-SELECT COUNT(DISTINCT Description) AS ValoresUnicos
-FROM [2017PurchasePricesDec_Clean];
+SELECT COUNT(DISTINCT PONumber) AS ValoresUnicos
+FROM InvoicePurchases12312016_Clean;
 
 SELECT Description, COUNT(*) AS Repeticiones
 FROM [2017PurchasePricesDec_Clean]
@@ -101,6 +101,7 @@ FROM [2017PurchasePricesDec_Clean];
 
 select*
 from Prov
+ORDER BY ProveedorID
 WHERE ProveedorID=10000;
 --order by ProveedorID
 
@@ -113,5 +114,81 @@ WHERE VendorNumber=10000;
 -------------------------------------------------------------------------------------------------
 --CREAR TABLA DE PRODUCTOS
 
+SELECT*
+FROM [2017PurchasePricesDec_Clean]
+--ORDER BY Description
 
 
+CREATE TABLE Productos (
+    ProductID INT IDENTITY,
+    Descripcion VARCHAR(250),
+    Size VARCHAR(50),
+    PurchasePrice FLOAT
+);
+
+INSERT INTO Productos(Descripcion, Size, PurchasePrice)
+SELECT DISTINCT Description, Size, PurchasePrice
+FROM [2017PurchasePricesDec_Clean];
+
+SELECT*
+FROM Productos
+
+-- Contar la cantidad de registros en la tabla PurchasePrices
+SELECT COUNT(*) AS TotalRegistros
+FROM PurchasesFINAL12312016_Clean;
+
+-- Contar la cantidad de valores únicos en la columna PONumber PurchasePrices
+SELECT COUNT(DISTINCT Description) AS ValoresUnicos
+FROM PurchasesFINAL12312016_Clean;
+
+
+
+-- Contar la cantidad de registros en la tabla PurchasePrices
+SELECT COUNT(*) AS TotalRegistros
+FROM PurchasesFINAL12312016_Clean;
+
+-- Contar la cantidad de valores únicos en la columna PONumber PurchasePrices
+SELECT COUNT (Description) AS ValoresUnicos
+FROM PurchasesFINAL12312016_Clean
+
+select*
+from SalesFINAL12312016_Clean
+
+
+select*
+from BegInvFINAL12312016_Clean
+
+-- Contar la cantidad de registros en la tabla PurchasePrices
+SELECT COUNT(*) AS TotalRegistros
+FROM BegInvFINAL12312016_Clean;
+
+-- Contar la cantidad de valores únicos en la columna PONumber PurchasePrices
+SELECT COUNT (InventoryId) AS ValoresUnicos
+FROM BegInvFINAL12312016_Clean
+
+
+select*
+from InvoicePurchases12312016_Clean
+
+SELECT COUNT(*) AS TotalRegistros
+FROM InvoicePurchases12312016_Clean;
+
+-- Contar la cantidad de valores únicos en la columna PONumber PurchasePrices
+SELECT COUNT (PONumber) AS ValoresUnicos
+FROM InvoicePurchases12312016_Clean
+
+-- Paso 1: Alterar la columna para que no permita valores nulos
+ALTER TABLE InvoicePurchases12312016_Clean
+ALTER COLUMN PONumber INT NOT NULL;
+
+-- Paso 3: Establecer la restricción de clave primaria
+ALTER TABLE InvoicePurchases12312016_Clean
+ADD CONSTRAINT PK_PONumber PRIMARY KEY (PONumber);
+
+
+select*
+from InvoicePurchases12312016_Clean
+
+SELECT *
+FROM dbo.InvoicePurchases12312016_Clean
+WHERE TRY_CAST(PONumber AS INT) IS NULL AND PONumber IS NOT NULL;
